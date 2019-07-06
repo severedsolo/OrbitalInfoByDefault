8d17673a83512658311d1f7211437a261ff50d85
severedsolo <severedsolo@users.noreply.github.com>
2019-07-06 14:24:27 +0100

    Initial commit

---
+++ OrbitalInfoByDefault.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using KSP.UI.Screens.Mapview;
+using UnityEngine;
+
+namespace OrbitalInfoByDefault
+{
+    public class OrbitalInfoByDefault : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+    }
+}
